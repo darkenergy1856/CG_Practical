af9c371084fa28e27de9071100cd9d95e5c6a8a1
Aryan Chandra <aryanchandra7360@gmail.com>
2023-01-19 19:30:22 +0530

    It Kind of Works.

---
+++ circle_midPoint.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <graphics.h>
+
+using namespace std;
+
+void drawPoint(int x, int y)
+{
+    putpixel(x, y, WHITE);
+    cout << x << "," << y << "\n";
+    putpixel(y, x, WHITE);
+    cout << y << "," << x << "\n";
+    putpixel(-x, y, WHITE);
+    cout << -x << "," << y << "\n";
+    putpixel(y, -x, WHITE);
+    cout << y << "," << -x << "\n";
+    putpixel(x, -y, WHITE);
+    cout << x << "," << -y << "\n";
+    putpixel(-y, x, WHITE);
+    cout << -y << "," << x << "\n";
+    putpixel(-x, -y, WHITE);
+    cout << -x << "," << -y << "\n";
+    putpixel(-y, -x, WHITE);
+    cout << -y << "," << -x << "\n";
+}
+
+int main()
+{
+    // int gd = DETECT, gm, color ;
+    // initgraph(&gd, &gm, NULL);
+    initwindow(960, 540);
+    int radius;
+    cout << "Enter Radius of the Circle : ";
+    cin >> radius;
+
+    float d = float(5 / 4) - radius;
+    int x = 0;
+    int y = radius;
+    drawPoint(x, y);
+    while (y > x)
+    {
+        if (d < 0)
+        {
+            d = d + 2 * x + 3;
+            x++;
+        }
+        else
+        {
+            d = d + 2 * x - 2 * y + 5;
+            x++;
+            y--;
+        }
+        drawPoint(x, y);
+    }
+
+    getch();
+    closegraph();
+    return 0;
+}
